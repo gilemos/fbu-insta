c07847bca66846f33e779610083df34a55caef35
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-10 16:39:17 -0700

    *Profile Cell Layout improved" Made image squared and did the dinamic layout

--- fbu-insta/View Controllers/ProfileViewController.m
+++ fbu-insta/View Controllers/ProfileViewController.m
@@ -53,7 +53,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     if(indexPath.row == 0) {
-        return 128;
+        return 86;
     }
     return 560;
 }
