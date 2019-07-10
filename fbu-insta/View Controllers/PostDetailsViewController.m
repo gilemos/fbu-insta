6dadea42b7932ecdf80a3de0cbc447e558d3906c
Giovanna Lemos Ribeiro <gilemos@fb.com>
2019-07-09 20:57:19 -0700

    *Time stamp* Time stamp label now working. Moreover, the layout is better

--- fbu-insta/View Controllers/PostDetailsViewController.m
+++ fbu-insta/View Controllers/PostDetailsViewController.m
@@ -15,8 +15,8 @@
 @property (weak, nonatomic) IBOutlet PFImageView *postImage;
 @property (weak, nonatomic) IBOutlet UIImageView *userProfileImage;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timeStampLabel;
 @property (weak, nonatomic) IBOutlet UITextField *caption;
-@property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
 @property (weak, nonatomic) IBOutlet UIButton *loveButton;
 @property (weak, nonatomic) IBOutlet UILabel *numberOfLovesLabel;
 
@@ -32,7 +32,7 @@
 -(void)refreshPost {
     //self.postImage.file = self.post.image;
     self.caption.text = self.post.caption;
-    self.timestampLabel.text = self.post.timeOfPosting.shortTimeAgoSinceNow;
+    self.timeStampLabel.text = self.post.timeOfPosting.shortTimeAgoSinceNow;
     self.numberOfLovesLabel.text = [NSString stringWithFormat:@"<3 %@ likes", self.post.likeCount];
 }
 /*
